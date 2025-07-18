8f9fa1ce92fcb20ef252564a97a3f4f7e685cce0
roam <adr.nicholas@gmail.com>
2025-07-18 18:49:15 -0400

    Optimizing CAN setup for 0-3-3:
    - manual mailbox setup
    - eliminated motor jitter
    - onCanMessage* for each CAN bus
    - disabled cyclic message except for encoder

--- include/Param.h
+++ include/Param.h
@@ -3,21 +3,34 @@
 #define ODRV0_CAN_NODE_ID 0
 #define ODRV1_CAN_NODE_ID 1
 #define ODRV2_CAN_NODE_ID 2
+#define ODRV3_CAN_NODE_ID 3
+#define ODRV4_CAN_NODE_ID 4
+#define ODRV5_CAN_NODE_ID 5
 
-#define N_CAN 3
+#define N_CAN 6
 
 #define N_ODRIVE_CAN1 3
-#define N_ODRIVE_CAN2 0
+#define N_ODRIVE_CAN2 3
 #define N_ODRIVE_CAN3 0
 
 #define ODRV0_CAN_ORDER_ID 0
 #define ODRV1_CAN_ORDER_ID 1
 #define ODRV2_CAN_ORDER_ID 2
+#define ODRV3_CAN_ORDER_ID 0
+#define ODRV4_CAN_ORDER_ID 1
+#define ODRV5_CAN_ORDER_ID 2
+
 
 #define ODRV0_CAN_BUS_ID 0
 #define ODRV1_CAN_BUS_ID 0
 #define ODRV2_CAN_BUS_ID 0
+#define ODRV3_CAN_BUS_ID 1
+#define ODRV4_CAN_BUS_ID 1
+#define ODRV5_CAN_BUS_ID 1
 
 #define ODRV0_CAN can1
 #define ODRV1_CAN can1
 #define ODRV2_CAN can1
+#define ODRV3_CAN can2
+#define ODRV4_CAN can2
+#define ODRV5_CAN can2
