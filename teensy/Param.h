ff7cff98ddddf3f0bc3cc92f90dcb3f46e889733
roam <adr.nicholas@gmail.com>
2025-07-23 16:49:22 -0400

    Working for 3-3-3

--- teensy/Param.h
+++ teensy/Param.h
@@ -6,12 +6,15 @@
 #define ODRV3_CAN_NODE_ID 3
 #define ODRV4_CAN_NODE_ID 4
 #define ODRV5_CAN_NODE_ID 5
+#define ODRV6_CAN_NODE_ID 6
+#define ODRV7_CAN_NODE_ID 7
+#define ODRV8_CAN_NODE_ID 8
 
-#define N_CAN 6
+#define N_CAN 9
 
 #define N_ODRIVE_CAN1 3
 #define N_ODRIVE_CAN2 3
-#define N_ODRIVE_CAN3 0
+#define N_ODRIVE_CAN3 3
 
 #define ODRV0_CAN_ORDER_ID 0
 #define ODRV1_CAN_ORDER_ID 1
@@ -19,6 +22,9 @@
 #define ODRV3_CAN_ORDER_ID 0
 #define ODRV4_CAN_ORDER_ID 1
 #define ODRV5_CAN_ORDER_ID 2
+#define ODRV6_CAN_ORDER_ID 0
+#define ODRV7_CAN_ORDER_ID 1
+#define ODRV8_CAN_ORDER_ID 2
 
 
 #define ODRV0_CAN_BUS_ID 0
@@ -27,6 +33,9 @@
 #define ODRV3_CAN_BUS_ID 1
 #define ODRV4_CAN_BUS_ID 1
 #define ODRV5_CAN_BUS_ID 1
+#define ODRV6_CAN_BUS_ID 2
+#define ODRV7_CAN_BUS_ID 2
+#define ODRV8_CAN_BUS_ID 2
 
 #define ODRV0_CAN can1
 #define ODRV1_CAN can1
@@ -34,3 +43,6 @@
 #define ODRV3_CAN can2
 #define ODRV4_CAN can2
 #define ODRV5_CAN can2
+#define ODRV6_CAN can3
+#define ODRV7_CAN can3
+#define ODRV8_CAN can3
