0fa524d113776bc46c1e27c956e61df8b22729c2
roam <adr.nicholas@gmail.com>
2025-07-25 16:42:21 -0400

    Add CAN packet rate benchmark for cyclic messages

---
+++ test/CAN_cyclic_msg_benchmark/Param.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#define ODRV0_CAN_NODE_ID 0
+#define ODRV1_CAN_NODE_ID 1
+#define ODRV2_CAN_NODE_ID 2
+#define ODRV3_CAN_NODE_ID 3
+#define ODRV4_CAN_NODE_ID 4
+#define ODRV5_CAN_NODE_ID 5
+#define ODRV6_CAN_NODE_ID 6
+#define ODRV7_CAN_NODE_ID 7
+#define ODRV8_CAN_NODE_ID 8
+
+#define N_CAN 9
+
+#define N_ODRIVE_CAN1 3
+#define N_ODRIVE_CAN2 3
+#define N_ODRIVE_CAN3 3
+
+#define ODRV0_CAN_ORDER_ID 0
+#define ODRV1_CAN_ORDER_ID 1
+#define ODRV2_CAN_ORDER_ID 2
+#define ODRV3_CAN_ORDER_ID 0
+#define ODRV4_CAN_ORDER_ID 1
+#define ODRV5_CAN_ORDER_ID 2
+#define ODRV6_CAN_ORDER_ID 0
+#define ODRV7_CAN_ORDER_ID 1
+#define ODRV8_CAN_ORDER_ID 2
+
+
+#define ODRV0_CAN_BUS_ID 0
+#define ODRV1_CAN_BUS_ID 0
+#define ODRV2_CAN_BUS_ID 0
+#define ODRV3_CAN_BUS_ID 1
+#define ODRV4_CAN_BUS_ID 1
+#define ODRV5_CAN_BUS_ID 1
+#define ODRV6_CAN_BUS_ID 2
+#define ODRV7_CAN_BUS_ID 2
+#define ODRV8_CAN_BUS_ID 2
+
+#define ODRV0_CAN can1
+#define ODRV1_CAN can1
+#define ODRV2_CAN can1
+#define ODRV3_CAN can2
+#define ODRV4_CAN can2
+#define ODRV5_CAN can2
+#define ODRV6_CAN can3
+#define ODRV7_CAN can3
+#define ODRV8_CAN can3
